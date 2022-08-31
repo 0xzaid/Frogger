17cc0c54573b167f453e0ab9b4076d8ea074f12c
zaid <zald0001@student.monash.edu>
2022-09-01 01:15:12 +0800

    beautified the code, and added some comments, some optimizations

--- src/main.ts
+++ src/main.ts
@@ -25,96 +25,92 @@ function main() {
    */
 
   
-
   /**
    * This is the view for your game to add and update your game elements.
    */
-  const svg = document.querySelector("#svgCanvas") as SVGElement & HTMLElement;
+   const svg = document.querySelector("#svgCanvas") as SVGElement & HTMLElement;
   
-  /* Document, canvas, & custom color information */
-  const Colors = {
-    RED: "#FF0000",
-    LIGHT_GREEN: "#90EE90",
-    LIGHT_BLUE: "#add8e6",
-    DARK_GREEN: "#228B22",
-  }
-  
-  // Changing page background to light green
-  document.body.style.background = Colors.LIGHT_GREEN;
 
+    /* All constant variables and types */
+    const CanvasConstants = {
+      canvasSize: {width: 800, height: 800},
+    }
+  
+    const FrogConstants = {
+      frogStartingPos: [390,750],
+      frogMovementSpeed: 35,
+      frogRadius: 15,
+    } as const;
+  
+    /* Enemy refers to obstacles or objects that could end the game when collided to */
+    const EnemyConstants = {
+      enemyMovementSpeed: 10,
+      enemySize: [45,100]
+    } as const;
+
+
+    /* Document, canvas, & custom color information */
+    const Colors = {
+      RED: "#FF0000",
+      LIGHT_GREEN: "#90EE90",
+      LIGHT_BLUE: "#add8e6",
+      DARK_GREEN: "#228B22",
+      GREEN_GRADIENT: "linear-gradient(to bottom, #339933 0%, #003300 100%)"
+    }
+
+    /* All interfaces are here */
+
+    /* Basic interface for all object variables */
+
+    interface IBody extends Circle, Rectangle{
+      ID: string,
+      shape: string,
+      speed: number,
+      size?: number[],
+      color?: string
+    }
+
+    /* All types are here */
+
+    type Circle = Readonly<{x_coord:number, y_coord:number}>;
+    type Rectangle = Readonly<{x_coord:number, pos_y:number}>;
+
+    type Key = "KeyA" | "KeyD" | "KeyW" | "KeyS";
+    type Event = "keydown" | "keyup";
+
+    type Body = Readonly<IBody>;
+
+    /* All classes are here */
+    
+    /* Game actions */
+    class Move{constructor(public readonly moveX: number, readonly moveY: number){}}
+    class Tick{constructor(public readonly timePassed: number){}}
+
+  /* Customizing the canvas and document */
+
+  // Changing page background to green gradient 
+  document.body.style.background = Colors.GREEN_GRADIENT;
+  
   // change canvas size
-  svg.setAttribute("width", "800");
-  svg.setAttribute("height", "800");
+  svg.setAttribute("width", `${CanvasConstants.canvasSize.width}`);
+  svg.setAttribute("height", `${CanvasConstants.canvasSize.height}`);
+
+  // getting the iconic frogger background image
+  svg.style.backgroundImage = "url('https://i.imgur.com/9TmTyuu.png')";
+  // making the background image the same size as the canvas
+  svg.style.backgroundSize = `${CanvasConstants.canvasSize.width}px ${CanvasConstants.canvasSize.height}px`;
+  // centering the canvas
+  svg.setAttribute("transform", `translate(540,-150)`);
   
-  // change canvas background to light blue
-  svg.style.background = Colors.LIGHT_BLUE; // light blue background
-  // image background
-  // svg.style.backgroundImage = "url('https://art.pixilart.com/f3a78cc98592f29.png')";
-  //svg.style.backgroundSize = "900px 600px";
-  svg.setAttribute("transform", "translate(550,-50)");
-
-  /* Game actions */
-
-  class Move{constructor(public readonly moveX: number, readonly moveY: number){}}
-  class Tick{constructor(public readonly timePassed: number){}}
-
-  /* Game constant variables and types */
-
-
-  const CanvasConstants = {
-    canvasSize: 900
-  }
-
-  const FrogConstants = {
-    frogStartingPos: [390,750],
-    frogMovementSpeed: 20,
-    frogRadius: 20,
-  } as const;
-
-  /* Enemy refers to obstacles or objects that could end the game when collided to */
-
-  const EnemyConstants = {
-    enemyMovementSpeed: 10,
-    enemySize: [45,100]
-  } as const;
-
-  /* Shape types and their details*/
-
-  type Circle = Readonly<{pos_x:number, pos_y:number}>;
-  type Rectangle = Readonly<{pos_x:number, pos_y:number}>;
-
-  /* Basic interface for all object variables */
-
-  interface IBody extends Circle, Rectangle{
-    ID: string,
-    shape: string,
-    speed: number,
-    size?: number[],
-    color?: string
-  }
-  // custom type that uses IBody interface for frog and enemies
-  type Body = Readonly<IBody>;
-
-
-    /* Creating the frog that will be controlled by user */
-//     function createFrog(): Body
-//     {
-//      return{
-//      ID: "frog",
-//      shape: "ellipse",
-//      pos_x: FrogConstants.frogStartingPos[0],
-//      pos_y: FrogConstants.frogStartingPos[1],
-//      speed: 0
-//      //color: Colors.DARK_GREEN,
-//    };
-//  }
-
+  /* Frog class */
 const createFrog = () => <Body>{
      ID: "frog",
-     shape: "ellipse",
-     pos_x: FrogConstants.frogStartingPos[0],
-     pos_y: FrogConstants.frogStartingPos[1],
+     shape: "circle",
+     x_coord: FrogConstants.frogStartingPos[0],
+     y_coord: FrogConstants.frogStartingPos[1],
      speed: 0,
+     color: Colors.LIGHT_GREEN,
+     size: [FrogConstants.frogRadius, FrogConstants.frogRadius]
 }
  
  /* Creating the enemies/obstacles that could end the game for the user */
@@ -124,8 +120,8 @@ const createFrog = () => <Body>{
  <Body>{
      ID: enemyId + (x + ""),
      shape: shape,
-     pos_x: x,
-     pos_y: y,
+     x_coord: x,
+     y_coord: y,
      speed: speed,
      size: size,
      color: color
@@ -147,31 +143,26 @@ const createFrog = () => <Body>{
     frog: createFrog(),
     /* enemy parameters: (x,y,speed,id,shape)(color, size)*/
     enemy: [
-      createEnemy(35, 500, 1, "enemy", "rect")("yellow", EnemyConstants.enemySize),
-      createEnemy(20, 400, 1, "enemy", "rect")("red", EnemyConstants.enemySize),
-      createEnemy(200, 300, 1, "enemy", "rect")("black", EnemyConstants.enemySize),
-      createEnemy(100, 200, 1, "enemy", "rect")("red", EnemyConstants.enemySize),
-      createEnemy(-10, 130, 1, "enemy", "rect")("red", EnemyConstants.enemySize),
-      createEnemy(500, 50, 1, "enemy", "rect")("brown", EnemyConstants.enemySize)
+      createEnemy(100, 630, 2, "enemy", "rect")("yellow", EnemyConstants.enemySize),
+      createEnemy(500, 570, 3, "enemy", "rect")("red", EnemyConstants.enemySize),
+      createEnemy(300, 450, 4, "enemy", "rect")("yellow", EnemyConstants.enemySize),
+      createEnemy(400, 330, 3, "enemy", "rect")("red", EnemyConstants.enemySize),
+      createEnemy(200, 215, 5, "enemy", "rect")("yellow", EnemyConstants.enemySize),
+      createEnemy(600, 95, 7, "enemy", "rect")("red", EnemyConstants.enemySize),
+
+      createEnemy(100, 330, 3, "enemy", "rect")("red", EnemyConstants.enemySize),
+      createEnemy(600, 215, 5, "enemy", "rect")("yellow", EnemyConstants.enemySize),
+      createEnemy(100, 95, 7, "enemy", "rect")("red", EnemyConstants.enemySize),
+
+      createEnemy(151, 630, 2, "enemy", "rect")("yellow", EnemyConstants.enemySize),
+      createEnemy(150, 570, 3, "enemy", "rect")("red", EnemyConstants.enemySize),
+      createEnemy(670, 450, 4, "enemy", "rect")("yellow", EnemyConstants.enemySize),
     ],
     GAME_OVER: false,
   }
 
 
-  const currentState = (stateNow: GameState, eventNow: Move | Tick ) => 
-    eventNow instanceof Move ? 
-    {...stateNow,
-      frog: 
-      { ...stateNow.frog,
-        pos_x: stateNow.frog.pos_x + eventNow.moveX,
-        pos_y: stateNow.frog.pos_y + eventNow.moveY
-      }
-    }
-    : tick(stateNow, eventNow.timePassed) 
-
-    /* Enemy and obstacles movements */
-
-    
+    /* tick function */
     const tick = (stateNow: GameState , timePassed: number) =>
     {
       return {...stateNow,
@@ -180,13 +171,21 @@ const createFrog = () => <Body>{
       };
     }
 
-    type Key = "KeyA" | "KeyD" | "KeyW" | "KeyS";
-    type Event = "keydown" | "keyup";
+    const currentState = (stateNow: GameState, eventNow: Move | Tick ) => 
+    eventNow instanceof Move ? 
+    {...stateNow,
+      frog: 
+      { ...stateNow.frog,
+        x_coord: stateNow.frog.x_coord + eventNow.moveX,
+        y_coord: stateNow.frog.y_coord + eventNow.moveY
+      }
+    }
+    : tick(stateNow, eventNow.timePassed) 
 
 
-    const
-    // FPS setter
-    gameClock = interval(10).pipe(map(timePassed => new Tick(timePassed))),
+ 
+    // FPS setter 
+    const gameClock = interval(10).pipe(map(timePassed => new Tick(timePassed))),
 
       /* Frog controls/movement */
 
@@ -206,26 +205,11 @@ const createFrog = () => <Body>{
     const moveBody = (enemy: Body) => <Body>
     {
       ...enemy,
-      pos_x: Number(enemy.pos_x) > 800 ? 790-enemy.pos_x + enemy.speed : 1+enemy.pos_x + enemy.speed,
+      x_coord: Number(enemy.x_coord) > 800 ? 790-enemy.x_coord + enemy.speed : 1+enemy.x_coord + enemy.speed,
       
     }
 
-    // interval(-10).subscribe(_ => enemy.setAttribute("x", String(1 + Number(enemy.getAttribute("x"))>800 ?
-    //  800-Number(enemy.getAttribute("x")): 1 + Number(enemy.getAttribute("x")))));
   
-
-
-    /* tick function */
-
-  const mainGameStream = merge(
-    gameClock,
-    moveLeft,
-    moveRight,
-    moveUp,
-    moveDown,
-  ).pipe(scan(currentState, initialState)).subscribe(updateView);
-
-
   function updateView(main_state: GameState){
     
     const updateBodyView = (body: Body, canvas: HTMLElement) => {
@@ -239,8 +223,8 @@ const createFrog = () => <Body>{
         
         //Different steps for different shapes
         body.shape == "rect" ?
-        (updateBody.setAttribute("x", String(body.pos_x)),
-        updateBody.setAttribute("y", String(body.pos_y)),
+        (updateBody.setAttribute("x", String(body.x_coord)),
+        updateBody.setAttribute("y", String(body.y_coord)),
         //Size is an optional variable
         body.size ?
           (updateBody.setAttribute("width", String(body.size[1])), 
@@ -260,27 +244,26 @@ const createFrog = () => <Body>{
       }
       
 
-    //Updating ellipses
-    const updateEllipse = () =>
+    //Updating circle objects
+    const moveFrog = () =>
     {
-      updateBody.setAttribute("cx", String(body.pos_x))
-      updateBody.setAttribute("cy", String(body.pos_y))
+      updateBody.setAttribute("cx", String(body.x_coord))
+      updateBody.setAttribute("cy", String(body.y_coord))
       updateBody.setAttribute("rx", String(FrogConstants.frogRadius))  
       updateBody.setAttribute("ry", String(FrogConstants.frogRadius))
     }
 
-    //Updating rects
-    const updatingRect = () =>
+    // Updating enemies
+    const moveEnemies = () =>
     {
-      updateBody.setAttribute("x", String(body.pos_x))
-      updateBody.setAttribute("y", String(body.pos_y))
-      updateBody.setAttribute("width", String(EnemyConstants.enemySize[1]))   
-      updateBody.setAttribute("height", String(EnemyConstants.enemySize[0]))
+      updateBody.setAttribute("x", String(body.x_coord))
+      updateBody.setAttribute("y", String(body.y_coord))
     }
 
     const updateBody = document.getElementById(body.ID) || createBodyView()
 
-    body.shape == "ellipse" ? updateEllipse() : updatingRect()
+    // only circle is the frog
+    body.shape == "circle" ? moveFrog() : moveEnemies()
     }
 
     // Update FROG
@@ -291,25 +274,17 @@ const createFrog = () => <Body>{
   
   }
 
-
-  // function showKeys() {
-  //   function showKey(k:Key) {
-  //     const arrowKey = document.getElementById(k)!,
-  //       o = (e:Event) => fromEvent<KeyboardEvent>(document,e).pipe(
-  //         filter(({code})=>code === k))
-  //     o('keydown').subscribe(e => arrowKey.classList.add("highlight"))
-  //     o('keyup').subscribe(_=>arrowKey.classList.remove("highlight"))
-  //   }
-  //   showKey('W');
-  //   showKey('A');
-  //   showKey('S');
-  //   showKey('D');
-  // }
-  
-  // setTimeout(showKeys, 0)
+  /* merge into 1 stream */
+  const mainGameStream = merge(
+    gameClock,
+    moveLeft,
+    moveRight,
+    moveUp,
+    moveDown,
+  ).pipe(scan(currentState, initialState)).subscribe(updateView);
 
 }
-    /* merge into 1 stream */
+    
 
 // The following simply runs your main function on window load.  Make sure to leave it in place.
 if (typeof window !== "undefined") {
